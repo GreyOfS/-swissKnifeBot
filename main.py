122a82b2b2d06dd0bcb2bfde88df03e68337d37e
Jean André André <72016452+GreyOfS@users.noreply.github.com>
2021-11-09 07:34:06 +0000

    ────────────────────

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def on_message(message):
 		return
 
 	if isSayHello(message.content):
-		print(f"👤╭─{message.author.name}\n📥╰─➤ \t{message.content}")
+		print(f"👤╭─{message.author.name}\n📥╰─➤ \t{message.content}\n────────────────────")
 		await message.channel.send(f"Hello {message.author.mention}!")
 
 sk.run(os.environ['D_TOKEN'])
