af7c4fbdf7798ad201888488748246449f9aa591
Jean André André <72016452+GreyOfS@users.noreply.github.com>
2021-11-15 07:05:12 +0000

    New v

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
-import os
-
 from replyHello import hello
+import re
+import os
 
 sk = discord.Client()
 
@@ -15,8 +15,14 @@ async def on_message(message):
 	if message.author == sk.user:
 		return
 
-	if hello.isSay(message.content):
+	if hello.isSay(hello, message.content):
 		print(f"👤╭─{message.author.name}\n📥╰─➤ \t{message.content}\n────────────────────")
 		await message.channel.send(f"Hello {message.author.mention}!")
+	
+	if re.compile(r'\b({0})\b'.format("never gonna give you up"), flags=re.IGNORECASE).search(message.content) != None:
+			await message.channel.send(
+				content = "~~https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=cameronbarnett~~",
+				delete_after = 60.0
+			)
 
 sk.run(os.environ['D_TOKEN'])
--- replyHello.py
+++ replyHello.py
@@ -2,13 +2,13 @@ import re
 import json
 
 ##
- # Gestion du dictionnaire des bonjour
+#	Gestion du dictionnaire des bonjour
 ##
 class dict_hello:
 	dictionary = None
 	hello = []
 	
-	def __init__(self, file="hello.json"):
+	def __init__(self, file:str="hello.json"):
 		with open(file) as hello_file:
 			self.dictionary = json.load(hello_file)
 		for i in self.dictionary.values():
@@ -18,19 +18,30 @@ class dict_hello:
 class hello:
 	hello_dict = dict_hello()
 
-	def learn(self, string, language): #v 0.1.0
-		if self.hello_dict.get(language) == None:
-			return 1
+	##
+	#	Permet d'apprendre une variante de salutation dans la langue demander ou dans une langue a créer
+	#	@param	slef
+	#	@param	word:str | Mot à apprendre
+	#	@param	language:str | Langue de $word
+	#	@return	bool | True si l'apprentissage a reussis et False si ça a échoué
+	##
+	def learn(self, word:str, language:str):
+		if language in self.hello_dict.dictionary.keys():
+
+			return True
 		else:
-			self.hello_dict.get(language).append(string)
-			return 0
+			print(f"\"{language}\" n'exite pas veuillez choisire une autre langue ou l'ajouter")
+			return False
+
 	##
 	#	Dit si la phrase dans la string est une salutaion à partire du dictionnaire HELLO
-	#	@param	str message = string à annalyser
+	#	@param	self
+	#	@param	message:str = string à annalyser
 	#	@return	bool
 	##
-	def isSay(self, message):
+	def isSay(self, message:str):	
 		isDetect = False
+
 		for possibility in self.hello_dict.hello:
 			if re.compile(r'\b({0})\b'.format(possibility), flags=re.IGNORECASE).search(message) != None:
 				isDetect = True
